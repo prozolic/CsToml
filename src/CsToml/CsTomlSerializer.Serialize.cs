0b5c7c1bc70038cba3faa4e7c6320f0e5784b434
prozolic <42107886+prozolic@users.noreply.github.com>
2024-01-20 10:16:17 +0900

    serialize

--- src/CsToml/CsTomlSerializer.Serialize.cs
+++ src/CsToml/CsTomlSerializer.Serialize.cs
@@ -1,8 +1,9 @@
 ﻿using CsToml.Utility;
+using CsToml.Values;
 using System.Buffers;
+using System.Runtime.InteropServices;
 
 namespace CsToml;
-
 public partial class CsTomlSerializer
 {
     public static byte[] Serialize(ref CsTomlPackage package)
@@ -17,18 +18,57 @@ public partial class CsTomlSerializer
         var utf8Writer = new Utf8Writer(bufferWriter);
         var csTomlWriter = new CsTomlWriter(ref utf8Writer);
 
-        var rootNode = package.Node;
-        foreach(var (key, childNode) in rootNode.Nodes)
-        {
-            if (!childNode.IsGroupingProperty)
-            {
-                csTomlWriter.WriteKeyValue(key, childNode.Value!);
-            }
-
-            // TODO: currently being implemented
-        }
+        SerializeValue(ref csTomlWriter, package.Node);
     }
 
+    private static void SerializeValue(ref CsTomlWriter writer, CsTomlTableNode node)
+    {
+        var keys = new List<Utf8FixString>();
+        SerializeValueCore(ref writer, node, keys);
+    }
+
+    private static void SerializeValueCore(ref CsTomlWriter writer, CsTomlTableNode node, List<Utf8FixString> keys)
+    {
+        foreach (var (key, childNode) in node.Nodes)
+        {
+            if (childNode.IsGroupingProperty)
+            {
+                if (node.IsTableHeader && !childNode.IsTableHeader && keys.Count > 0)
+                {
+                    var keysSpan = CollectionsMarshal.AsSpan(keys);
+                    writer.WriteTableHeader(keysSpan);
+                    keys.Clear();
+                }
+                keys.Add(key);
+                SerializeValueCore(ref writer, childNode, keys);
+                continue;
+            }
+            else
+            {
+                if (node.IsTableHeader && keys.Count > 0)
+                {
+                    var keysSpan = CollectionsMarshal.AsSpan(keys);
+                    writer.WriteTableHeader(keysSpan);
+                    keys.Clear();
+                    writer.WriteKeyValue(in key, childNode.Value!);
+                }
+                else
+                {
+                    var keysSpan = CollectionsMarshal.AsSpan(keys);
+                    if (keysSpan.Length > 0)
+                    {
+                        for (var i = 0; i < keysSpan.Length; i++)
+                        {
+                            writer.WriterKey(in keysSpan[i], true);
+                        }
+                    }
+                    writer.WriteKeyValue(in key, childNode.Value!);
+                }
+            }
+        }
+
+        keys.Clear(); // clear subkey
+    }
 
 
 }
--- src/CsToml/CsTomlWriter.cs
+++ src/CsToml/CsTomlWriter.cs
@@ -1,27 +1,73 @@
 ﻿using CsToml.Formatter;
 using CsToml.Utility;
 using CsToml.Values;
+using System.Buffers;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace CsToml;
 
 internal ref struct CsTomlWriter
 {
     private Utf8Writer writer;
+    private readonly ReadOnlySpan<byte> newLineCh;
+
+    private ReadOnlySpan<byte> currentKeySpan = [];
 
     [DebuggerStepThrough]
     public CsTomlWriter(ref Utf8Writer bufferWriter)
     {
         writer = bufferWriter;
+        newLineCh = OperatingSystem.IsWindows() ? CsTomlSyntax.Symbol.WindowsNewLine : CsTomlSyntax.Symbol.UnixNewLine;
     }
 
-    public void WriteKeyValue(Utf8FixString key, CsTomlValue value)
+    public void WriteNewLine()
     {
-        writer.Write(key.BytesSpan);
-        writer.Write(" = "u8);
-
-        value.ToTomlString(ref writer);
-        writer.Write("\n"u8);
+        writer.Write(newLineCh);
     }
 
+    public void WriteKeyValue(in Utf8FixString key, CsTomlValue value)
+    {
+        WriterKey(in key, false);
+        WriteEquals();
+        WriterValue(in key, value);
+    }
+
+    public void WriterKey(in Utf8FixString key, bool isGroupingProperty)
+    {
+        if (isGroupingProperty)
+        {
+            writer.Write(key.BytesSpan);
+            writer.Write(CsTomlSyntax.Symbol.PERIOD);
+        }
+        else
+        {
+            writer.Write(key.BytesSpan);
+        }
+    }
+
+    public void WriteEquals()
+    {
+        writer.Write(" = "u8);
+    }
+
+    public void WriterValue(in Utf8FixString key, CsTomlValue value)
+    {
+        value.ToTomlString(ref writer);
+        writer.Write(newLineCh);
+    }
+
+    public void WriteTableHeader(Span<Utf8FixString> keysSpan)
+    {
+        writer.Write(CsTomlSyntax.Symbol.LEFTSQUAREBRACKET);
+        if (keysSpan.Length > 0)
+        {
+            for (var i = 0; i < keysSpan.Length; i++)
+            {
+                WriterKey(in keysSpan[i], i < keysSpan.Length - 1);
+            }
+        }
+        writer.Write(CsTomlSyntax.Symbol.RIGHTSQUAREBRACKET);
+        WriteNewLine();
+    }
 }
