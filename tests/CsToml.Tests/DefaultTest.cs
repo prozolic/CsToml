b21a7ba8082fc5b143510962f1dce2ac2dd80f51
prozolic <42107886+prozolic@users.noreply.github.com>
2024-02-18 01:09:46 +0900

    add test

---
+++ tests/CsToml.Tests/DefaultTest.cs
@@ -0,0 +1,81 @@
+﻿using CsToml.Error;
+
+namespace CsToml.Tests;
+
+public class DefaultTest
+{
+    private readonly byte[] tomlText;
+
+    public DefaultTest()
+    {
+        tomlText = @"
+key = ""value""
+first.second.third = ""value""
+number = 123456
+
+"u8.ToArray();
+
+    }
+
+    [Fact]
+    public void DeserializeAndSerializeTest()
+    {
+        var package = new CsTomlPackage();
+        CsTomlSerializer.Deserialize(ref package, tomlText);
+
+        var serializeText = CsTomlSerializer.Serialize(ref package);
+    }
+
+    [Fact]
+    public void TryGetValueTest()
+    {
+        var package = new CsTomlPackage();
+        CsTomlSerializer.Deserialize(ref package, tomlText);
+
+        {
+            var result = package.TryGetValue("key"u8, out var value);
+            Assert.True(result);
+            Assert.Equal("value", value?.GetString());
+        }
+        {
+            var result = package.TryGetValue("first.second.third"u8, out var value);
+            Assert.True(result);
+            Assert.Equal("value", value?.GetString());
+        }
+        {
+            var result = package.TryGetValue("number"u8, out var value);
+            Assert.True(result);
+            Assert.Equal(123456, value?.GetInt64());
+        }
+        {
+            var result = package.TryGetValue("failed"u8, out var value);
+            Assert.False(result);
+            Assert.Null(value);
+        }
+    }
+
+    [Fact]
+    public void FindTest()
+    {
+        var package = new CsTomlPackage();
+        CsTomlSerializer.Deserialize(ref package, tomlText);
+
+        {
+            var value = package.Find(["key"u8]);
+            Assert.Equal("value", value?.GetString());
+        }
+        {
+            var value = package.Find(["first"u8, "second"u8, "third"u8]);
+            Assert.Equal("value", value?.GetString());
+        }
+        {
+            var value = package.Find(["number"u8]);
+            Assert.Equal(123456, value?.GetInt64());
+        }
+        {
+            var value = package.Find(["failed"u8]);
+            Assert.Throws<CsTomlException>(() => { var _ = value.GetString(); });
+        }
+    }
+}
+
---
+++ tests/CsToml.Tests/Toml-Test.cs
@@ -0,0 +1,76 @@
+namespace CsToml.Tests;
+
+/// <summary>
+/// Verify with the verification Toml files.
+/// The 'toml-test' repository is borrowed for the verification Toml files.
+/// https://github.com/toml-lang/toml-test
+/// </summary>
+public class TomlTest
+{
+    private static readonly string tomlTestDirectoryPath = "./../../../toml-test/";
+    private static readonly string ValidDirectory = "valid";
+    private static readonly string InvalidDirectory = "invalid";
+    private static readonly string TomlExtension = ".toml";
+    private static readonly string TomlFilesVer100 = "files-toml-1.0.0";
+
+    [Theory, MemberData(nameof(ValidTomlFile))]
+    public void ValidTest(string tomlFile)
+    {
+        var package = new CsTomlPackage
+        {
+            IsThrowCsTomlException = false
+        };
+        CsTomlSerializer.ReadAndDeserialize(ref package, tomlFile);
+
+        if (package.Exceptions.Count > 0)
+        {
+            Assert.Fail(package.Exceptions[0]!.InnerException!.ToString());
+        }
+    }
+
+    [Theory, MemberData(nameof(InvalidTomlFile))]
+    public void InvalidTest(string tomlFile)
+    {
+        var package = new CsTomlPackage
+        {
+            IsThrowCsTomlException = false
+        };
+        CsTomlSerializer.ReadAndDeserialize(ref package, tomlFile);
+
+        if (package.Exceptions.Count == 0)
+        {
+            Assert.Fail($@"{tomlFile}");
+        }
+    }
+
+    public static IEnumerable<object[]> ValidTomlFile()
+    {
+        var filesToml = Path.Combine(tomlTestDirectoryPath, TomlFilesVer100);
+        var files = File.ReadAllLines(filesToml);
+        foreach (var file in files)
+        {
+            var directoryName = file.Split('/')[0];
+            var tomlFile = new FileInfo(Path.Combine(tomlTestDirectoryPath, file));
+            if (tomlFile.Exists && tomlFile.Extension == TomlExtension && directoryName == ValidDirectory)
+            {
+                yield return new object[] { Path.Combine(tomlTestDirectoryPath, file) };
+            }
+        }
+    }
+
+    public static IEnumerable<object[]> InvalidTomlFile()
+    {
+        var filesToml = Path.Combine(tomlTestDirectoryPath, TomlFilesVer100);
+        var files = File.ReadAllLines(filesToml);
+        foreach (var file in files)
+        {
+            var directoryName = file.Split('/')[0];
+            var tomlFile = new FileInfo(Path.Combine(tomlTestDirectoryPath, file));
+            if (tomlFile.Exists && tomlFile.Extension == TomlExtension && directoryName == InvalidDirectory)
+            {
+                yield return new object[] { Path.Combine(tomlTestDirectoryPath, file) };
+
+            }
+        }
+    }
+}
