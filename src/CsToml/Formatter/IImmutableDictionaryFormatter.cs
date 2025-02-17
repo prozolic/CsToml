18bffea32e898145b48fec3c7d99a4b61cc48221
prozolic <42107886+prozolic@users.noreply.github.com>
2025-02-17 22:49:16 +0900

    Add Immutable collection interface and fix bug

---
+++ src/CsToml/Formatter/IImmutableDictionaryFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+
+namespace CsToml.Formatter;
+
+public sealed class IImmutableDictionaryFormatter<TKey, TValue> : DictionaryBaseFormatter<TKey, TValue, IImmutableDictionary<TKey, TValue>, Dictionary<TKey, TValue>>
+    where TKey : notnull
+{
+    protected override void AddValue(Dictionary<TKey, TValue> mediator, TKey key, TValue value)
+    {
+        mediator.Add(key, value);
+    }
+
+    protected override IImmutableDictionary<TKey, TValue> Complete(Dictionary<TKey, TValue> dictionary)
+    {
+        return dictionary.ToImmutableDictionary();
+    }
+
+    protected override Dictionary<TKey, TValue> CreateMediator(int capacity)
+    {
+        return new Dictionary<TKey, TValue>(capacity);
+    }
+
+}
